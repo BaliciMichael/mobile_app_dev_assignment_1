e54b302a12b8fe5ec2e3a13f84c5df3050ef8247
BaliciMichael <balicimichael@gmail.com>
2023-02-16 11:10:45 +0000

    Added Data Validation to the AddPlayer class

--- app/src/main/java/ie/setu/assignment1/activities/AddPlayer.kt
+++ app/src/main/java/ie/setu/assignment1/activities/AddPlayer.kt
@@ -1,13 +1,13 @@
 package ie.setu.assignment1.activities
 
+import android.animation.ObjectAnimator
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log.i
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.content.ContextCompat
 import ie.setu.assignment1.R
-import ie.setu.assignment1.databinding.ActivityAddPlayerBinding
 import ie.setu.assignment1.main.MainApp
 import ie.setu.assignment1.models.Player
 
@@ -15,7 +15,7 @@ import ie.setu.assignment1.models.Player
 class AddPlayer : AppCompatActivity() {
     //private val players = ArrayList<Player>()
 
-    private lateinit var binding: ActivityAddPlayerBinding
+  //  private lateinit var binding: ActivityAddPlayerBinding
     var player = Player()
     var app : MainApp? = null
 
@@ -83,25 +83,59 @@ class AddPlayer : AppCompatActivity() {
             val mvpCheckBox = findViewById<CheckBox>(R.id.mvp_checkbox)
             val mvpNumEditText = findViewById<EditText>(R.id.mvpNum)
 
-            val playerName = playerNameEditText.text.toString()
-            val lastname = playerLastName.text.toString()
-            val playerAgeStr = playerAgeEditText.text.toString()
-            val playerAge = if (playerAgeStr.isNullOrEmpty()) 0 else playerAgeStr.toInt()
+            val playerName = playerNameEditText.text.toString().trim()
+            val lastname = playerLastName.text.toString().trim()
+            val playerAgeStr = playerAgeEditText.text.toString().trim()
+            var playerAge = 0
 
+            try {
+                playerAge = playerAgeStr.toInt()
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "Please enter a valid number for age", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
 
-
-                val nationality = nationalityEditText.text.toString()
-                val mvp = mvpCheckBox.isChecked
-
-                val numOfMvp = if (!mvp) 0 else mvpNumEditText.text.toString().toInt()
-
-                val club = clubSpinner.selectedItem.toString()
-                val position = positionSpinner.selectedItem.toString()
-                if (playerName.isEmpty() || club.isEmpty() || position.isEmpty() || lastname.isEmpty() || playerAge <= 0 || nationality.isEmpty()) {
-                    Toast.makeText(this, "Please enter all values", Toast.LENGTH_SHORT).show()
-                } else {
-                    createPlayer(playerName, lastname, playerAge, nationality, mvp, numOfMvp, club, position)
+            val nationality = nationalityEditText.text.toString().trim()
+            val mvp = mvpCheckBox.isChecked
+            var numOfMvp = 0// temporary fix
+            if(mvp) {
+                try {
+                    numOfMvp = mvpNumEditText.text.toString().toInt()
+                } catch (e: NumberFormatException) {
+                    Toast.makeText(this, "Please enter a valid number of MVPs", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
                 }
+            }
+
+            val club = clubSpinner.selectedItem.toString()
+            val position = positionSpinner.selectedItem.toString()
+
+            if (playerName.isEmpty()) {
+                playerNameEditText.setHintTextColor(ContextCompat.getColor(this, R.color.red))
+                //println(ContextCompat.getColor(this,R.color.red).toString())
+                Toast.makeText(this, "Please Enter a Name", Toast.LENGTH_SHORT)
+                    .show()
+
+            }else if (lastname.isEmpty()) {
+                playerLastName.setHintTextColor(ContextCompat.getColor(this, R.color.red))
+                Toast.makeText(this, "Please Enter the Last Name", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (playerAge <= 0) {
+
+                playerAgeEditText.setHintTextColor(ContextCompat.getColor(this, R.color.red))
+                Toast.makeText(this, "Please a Valid Number", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (nationality.isEmpty()) {
+                nationalityEditText.setHintTextColor(ContextCompat.getColor(this, R.color.red))
+                Toast.makeText(this, "Please Enter a valid Nationality", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+             else {
+                createPlayer(playerName, lastname, playerAge, nationality, mvp, numOfMvp, club, position)
+            }
 
         }
     }
--- app/src/main/java/ie/setu/assignment1/activities/MainActivity.kt
+++ app/src/main/java/ie/setu/assignment1/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package ie.setu.assignment1.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import ie.setu.assignment1.databinding.ActivityMainBinding
 import ie.setu.assignment1.activities.AddPlayer
 import ie.setu.assignment1.main.MainApp
@@ -13,8 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         app = application as MainApp
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+       // val layoutManager = LinearLayoutManager(this)
+       // binding.recyclerView.layoutManager = layoutManager
+       // binding.recyclerView.adapter = PlacemarkAdapter(app.players)
 
         binding.addPlayerButton.setOnClickListener { val intent=Intent(this, AddPlayer::class.java)
                 startActivity(intent)
